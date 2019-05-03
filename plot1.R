b73d00588cd71d67fca7af67559fdef48d219722
EmeraldasHofmann <honjo_nana@yahoo.it>
2019-05-03 18:35:54 +0200

    upload R-script and plot in PNG format

---
+++ plot1.R
@@ -0,0 +1,40 @@
+library(dplyr)
+library(lubridate)
+
+
+file_download <- function(){
+  if(!file.exists("./data_energy")){dir.create("./data_energy/")}
+  
+  #download dataset in temporary file and unzip DB
+  fURLzip <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+  temp <- tempfile()
+  download.file(fURLzip, temp, method = "curl")
+  unzip(temp, exdir = "./data_energy/")
+  # rename file
+  file.rename("./data_energy/household_power_consumption.txt", "./data_energy/hpc.txt")
+  
+}
+
+make_dataset <- function(){
+  #sercing data file, if no exist call file_download() function
+  if(!file.exists("./data_energy/hpc.txt")){file_download()}
+  ## read dataset
+  hpc_data <- data.frame(read.table("./data_energy/hpc.txt", sep = ";", header = TRUE, stringsAsFactors = FALSE, colClasses = c("character", "character", "numeric", "numeric", "numeric", "numeric","numeric", "numeric", "numeric"), na.strings = "?"))
+  
+  # subsetting dataframe from 1/2/2007 to 2/2/2007
+  hpc_sub <- filter(hpc_data, Date == "1/2/2007" | Date == "2/2/2007")
+  rm("hpc_data")
+  hpc_sub
+}
+
+plot1 <- function(){
+  # serch to subset dataframe, if no exist in wd call make_dataset() function
+  if(sum(ls() == "hpc_sub") == 0){hpc_sub <- make_dataset()}
+  
+  # MAKE PLOT 1
+  png(filename = "./plot1.png", width = 480, height = 480)
+  hist(hpc_sub$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
+  dev.off()
+}
+
+
---
+++ plot2.R
@@ -0,0 +1,42 @@
+library(dplyr)
+library(lubridate)
+
+
+file_download <- function(){
+  if(!file.exists("./data_energy")){dir.create("./data_energy/")}
+  
+  #download dataset in temporary file and unzip DB
+  fURLzip <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+  temp <- tempfile()
+  download.file(fURLzip, temp, method = "curl")
+  unzip(temp, exdir = "./data_energy/")
+  # rename file
+  file.rename("./data_energy/household_power_consumption.txt", "./data_energy/hpc.txt")
+  
+}
+
+make_dataset <- function(){
+  #sercing data file, if no exist call file_download() function
+  if(!file.exists("./data_energy/hpc.txt")){file_download()}
+  ## read dataset
+  hpc_data <- data.frame(read.table("./data_energy/hpc.txt", sep = ";", header = TRUE, stringsAsFactors = FALSE, colClasses = c("character", "character", "numeric", "numeric", "numeric", "numeric","numeric", "numeric", "numeric"), na.strings = "?"))
+  
+  # subsetting dataframe from 1/2/2007 to 2/2/2007
+  hpc_sub <- filter(hpc_data, Date == "1/2/2007" | Date == "2/2/2007")
+  rm("hpc_data")
+  hpc_sub
+}
+
+
+plot2 <- function(){
+  # serch to subset dataframe, if no exist in wd call make_dataset() function
+  if(sum(ls() == "hpc_sub") == 0){hpc_sub <- make_dataset()}
+  datetime <- dmy_hms(paste(hpc_sub$Date, hpc_sub$Time))
+  
+  # MAKE PLOT 2
+  png(filename = "./plot2.png", width = 480, height = 480)
+  plot(datetime, hpc_sub$Global_active_power, type = "l",   xlab ="", ylab = "Global Active Power (kilowatts)")
+  dev.off()
+}
+
+
